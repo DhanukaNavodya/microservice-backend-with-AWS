02ff0ab9f6afd6eaa564b735c625b2ad1c527f2b
Dhanuka Navodya <162307420+DhanukaNavodya@users.noreply.github.com>
2026-02-23 00:27:15 +0530

    test auto deploy

--- services/orders/src/index.js
+++ services/orders/src/index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.get("/health", (req, res) => res.json({ ok: true, service: "orders" }));
 
 app.get("/orders", (req, res) => {
-  res.json([{ id: "ORD-1", total: 1200 }, { id: "ORD-2", total: 500 }]);
+  res.json([{ id: "ORD-1", total: 1200 }, { id: "ORD-2", total: 500 },{ id:"ORD-3", total: 1200 }, { id: "ORD-4", total: 500 },{ id:"ORD-5", total: 1200 }, { id: "ORD-6", total: 500 }]);
 });
 
 const port = process.env.PORT || 3002;
